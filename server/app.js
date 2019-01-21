6b4c21c3541d47780aa56c5cff286a5ceecefec8
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-20 17:23:35 -0800

    Update favicon to calendar icon

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const parser = require('body-parser');
+const favicon = require('serve-favicon');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(favicon(path.join(__dirname, '../client/public/assets/calendar-icon.png')));
 
 app.use(parser.json());
 app.use(morgan('dev'));
