b77858212ba114aceb6b0675114d071789ffa099
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-21 17:57:22 -0800

    Update css font-family & dynamic column height

--- client/src/utils/initial-data.js
+++ client/src/utils/initial-data.js
@@ -4,6 +4,7 @@ const initialData = {
     'task-2': { id: 'task-2', content: 'Watch TV' },
     'task-3': { id: 'task-3', content: 'Clean Apartment' },
     'task-4': { id: 'task-4', content: 'Read Book' },
+    'task-5': { id: 'task-5', content: 'Well I have a lot of things to do but I am not sure exactly how to do it' },
   },
   columns: {
     'column-1': {
@@ -14,12 +15,12 @@ const initialData = {
     'column-2': {
       id: 'column-2',
       title: 'Finished',
-      tasks: ['task-3', 'task-4'],
+      tasks: ['task-3', 'task-4', 'task-5'],
     },
   },
   columnOrder: ['column-1', 'column-2'],
-  taskCount: 2,
-  columnCount: 1,
+  taskCount: 5,
+  columnCount: 2,
 };
 
 export default initialData;
