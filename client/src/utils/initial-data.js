9f6ed1b5f802b37bab5af2d49f2f2d1443a0caf8
BalasundaramVi <vigneshbalasundaram@gmail.com>
2019-01-21 18:12:28 -0800

    Prepare initial data for click/drag functionality

--- client/src/utils/initial-data.js
+++ client/src/utils/initial-data.js
@@ -5,6 +5,10 @@ const initialData = {
     'task-3': { id: 'task-3', content: 'Clean Apartment' },
     'task-4': { id: 'task-4', content: 'Read Book' },
     'task-5': { id: 'task-5', content: 'Well I have a lot of things to do but I am not sure exactly how to do it' },
+    'task-6': { id: 'task-6', content: 'Write Code' },
+    'task-7': { id: 'task-7', content: 'Study HTTP Verbs' },
+    'task-8': { id: 'task-8', content: 'Read Cracking the Coding Interview' },
+    'task-9': { id: 'task-9', content: 'Take Nap' },
   },
   columns: {
     'column-1': {
@@ -17,10 +21,15 @@ const initialData = {
       title: 'Finished',
       tasks: ['task-3', 'task-4', 'task-5'],
     },
+    'column-3': {
+      id: 'column-3',
+      title: 'Long Term',
+      tasks: ['task-6', 'task-7', 'task-8', 'task-9'],
+    }
   },
-  columnOrder: ['column-1', 'column-2'],
-  taskCount: 5,
-  columnCount: 2,
+  columnOrder: ['column-1', 'column-2', 'column-3'],
+  taskCount: 9,
+  columnCount: 3,
 };
 
 export default initialData;
